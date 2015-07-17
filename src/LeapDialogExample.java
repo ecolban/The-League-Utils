4f3bc33c355e27bacaf6b3f33e7b9a2b49bc2278
ecolban <erik.colban@drawmetry.com>
2015-07-17 13:08:00 -0700

    InterruptedExceptions are caught and not thrown.

--- src/LeapDialogExample.java
+++ src/LeapDialogExample.java
@@ -5,46 +5,61 @@ public class LeapDialogExample {
 
 	public static void main(String[] args) {
 
-		testRun();
+		testGetInput1();
+		testGetInput2();
+		testShowMessage();
+		testChoose1();
+		testChoose2();
 
 	}
 
-	private static void testRun() {
-		try {
-			// Get some input from the user as a String:
+	private static void testGetInput1() {
+		// Get some input from the user as a String:
+		String name = LeapDialog.getInput("What's your first name:");
+		System.out.println(String.format("Your first name is %s.", name));
 
-			String name = LeapDialog.getInput("What's your first name:");
-			System.out.println(String.format("Your first name is %s.", name));
+	}
 
-			// For multi-line messages, use HTML:
-			name = LeapDialog
-			        .getInput("<html>Please,<br>tell me your last name:</html>");
-			System.out.println(String.format("Your last name is %s.", name));
+	private static void testGetInput2() {
 
-			// When no input from the user is expected:
-			LeapDialog.showMessage("Thanks!!");
+		// For multi-line messages, use HTML:
+		String question = "<html>Please,<br>tell me your last name:</html>";
+		String name = LeapDialog.getInput(question);
+		System.out.println(String.format("Your last name is %s.", name));
+	}
+	
+	private static void testShowMessage() {
+		// No input from the user is expected:
+		LeapDialog.showMessage("Thanks!!");
+	}
 
-			// Let the user respond with a button click (list the options in
-			// the preferred order):
-			Option.YES.setString("YEAH");
-			Option.NO.setString("DEFINITELY NOT!!!");
-			Option.MAY_BE.setString("NAH...");
-			Option.CANCEL.setString("LATER");
-			Option opt = LeapDialog.choose("Was that fun?",
-			        Option.YES, Option.MAY_BE, Option.NO, Option.CANCEL);
-			switch (opt) {
-			case YES:
-				System.out.println("I like positive people!");
-				break;
-			case NO:
-				System.out.println("Don't be so negative!");
-				break;
-			default:
-				System.out.println("Indecisive today?");
-			}
+	// Let the user respond with a button click (list the options in
+	// the preferred order):
+	private static void testChoose1() {
+		Option opt = LeapDialog.choose("Was that fun?",	Option.YES, Option.NO);
+		System.out.println(opt == Option.YES ? "Yeah!" : "Boo...");
+	}
 
-		} catch (InterruptedException e) {
-			System.out.println("Our dialog was interrupted.");
+	// Let the user respond with a button click (list the options in
+	// the preferred order):
+	private static void testChoose2() {
+		
+		Option.NO.setString("Definitely not!!!");
+		Option.MAY_BE.setString("nah...");
+		
+		Option opt = LeapDialog.choose("Wasn't that cool?!",
+				Option.YES, Option.MAY_BE, Option.NO, Option.CANCEL);
+	
+		switch (opt) {
+		case YES:
+			System.out.println("I like positive people!");
+			break;
+		case NO:
+			System.out.println("Don't be so negative!");
+			break;
+		default:
+			System.out.println("Indecisive today?");
 		}
 	}
+
 }
--- src/org/jointheleague/util/LeapDialog.java
+++ src/org/jointheleague/util/LeapDialog.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.SynchronousQueue;
+
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -27,7 +28,7 @@ public class LeapDialog {
 	 * Possible responses that a user may give by clicking a button.
 	 */
 	public enum Option {
-		YES, NO, MAY_BE, OK, CANCEL;
+		YES, NO, MAY_BE, OK, SKIP, CANCEL;
 
 		private String string;
 
@@ -95,34 +96,37 @@ public class LeapDialog {
 	 * 
 	 * @param msg
 	 *            The message that is presented to the user.
-	 * @throws InterruptedException
-	 *             if interrupted before the user has clicked OK.
 	 * 
 	 */
-	public static void showMessage(final String msg)
-	        throws InterruptedException {
-		new LeapDialog(msg, false);
+	public static void showMessage(final String msg) {
+		try {
+			new LeapDialog(msg, false);
+		} catch (InterruptedException e) {
+		}
 	}
 
 	/**
 	 * This method is used to ask the user for input. The method blocks until
-	 * the user clicks on the OK button.
+	 * the user clicks on the OK button. If interrupted, returns null.
 	 * 
 	 * @param msg
 	 *            A message asking the user for input.
+	 * @param options
+	 *            Options that the user may select, such as OK, SKIP or CANCEL
 	 * @return The answer provided by the user.
-	 * @throws InterruptedException
-	 *             if the dialog is interrupted before the user has clicked on
-	 *             the OK button.
 	 */
-	public static String getInput(final String msg) throws InterruptedException {
-		return new LeapDialog(msg, true).input;
+	public static String getInput(final String msg) {
+		try {
+			return new LeapDialog(msg, true).input;
+		} catch (InterruptedException e) {
+			return null;
+		}
 	}
 
 	/**
 	 * This method is used to request the user to make a choice. Possible
 	 * answers are provided in the Option enum. The method blocks until the user
-	 * clicks on the OK button.
+	 * clicks on the OK button. If interrupted, returns null.
 	 * 
 	 * @param msg
 	 *            The message asking the user to select an option
@@ -132,25 +136,27 @@ public class LeapDialog {
 	 *            be at least one option.
 	 * @return The option that the user chose. It is one of the options provided
 	 *         as argument to this method.
-	 * @throws InterruptedException
-	 *             if the dialog is interrupted before the user has clicked on a
-	 *             button.
+	 *         
 	 * @throws IllegalArgumentException
 	 *             if no options are provided.
 	 */
-	public static Option choose(final String msg, final Option... options)
-	        throws InterruptedException {
+	public static Option choose(final String msg, final Option... options) {
 		if (options.length == 0) {
 			throw new IllegalArgumentException(
 			        "There must be at least one option.");
 		}
-		return new LeapDialog(msg, options).returnOption;
+		try {
+	        return new LeapDialog(msg, options).returnOption;
+        } catch (InterruptedException e) {
+        	return null;
+        }
 	}
 
 	private void buildMessageGUI(final BlockingQueue<Option> queue,
 	        String message) {
 		final JDialog d = new JDialog();
-		d.setMinimumSize(new Dimension(400, 100));
+		d.setMinimumSize(new Dimension(400, 66));
+		d.setLocationRelativeTo(null);
 		d.setLayout(new BorderLayout());
 		JPanel msgPanel = new JPanel();
 		msgPanel.add(new JLabel(message));
@@ -176,15 +182,18 @@ public class LeapDialog {
 	        String message) {
 		final JDialog d = new JDialog();
 		d.setMinimumSize(new Dimension(400, 100));
+		d.setLocationRelativeTo(null);
 		d.setLayout(new BorderLayout());
 		JPanel msgPanel = new JPanel();
 		msgPanel.add(new JLabel(message));
 		d.add(msgPanel, BorderLayout.NORTH);
-		inputField = new JTextField(20);
-		d.add(inputField, BorderLayout.CENTER);
+		JPanel inputPanel = new JPanel();
+		inputField = new JTextField(40);
+		inputPanel.add(inputField);
+		d.add(inputPanel, BorderLayout.CENTER);
 		JPanel buttonPanel = new JPanel();
-		JButton okButton = new JButton(Option.OK.toString());
-		okButton.addActionListener(new ActionListener() {
+		JButton b = new JButton(Option.OK.toString());
+		b.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -194,7 +203,7 @@ public class LeapDialog {
 				queue.offer(Option.OK);
 			}
 		});
-		buttonPanel.add(okButton);
+		buttonPanel.add(b);
 		d.add(buttonPanel, BorderLayout.SOUTH);
 		d.pack();
 		d.setVisible(true);
@@ -203,7 +212,8 @@ public class LeapDialog {
 	private void buildChoiceGUI(final BlockingQueue<Option> queue,
 	        String message, Option... options) {
 		final JDialog d = new JDialog();
-		d.setPreferredSize(new Dimension(500, 100));
+		d.setMinimumSize(new Dimension(400, 66));
+		d.setLocationRelativeTo(null);
 		d.setLayout(new BorderLayout());
 		JPanel msgPanel = new JPanel();
 		msgPanel.add(new JLabel(message));
